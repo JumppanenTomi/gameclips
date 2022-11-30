ee0dc2eeab7b43842babe3432f6a80da2ca41370
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-11-30 13:04:17 +0200

    added video route, cors etc.

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-'use strict'
-console.log('moikka')
+'use strict';
+const express=require('express');
+const app=express();
+const cors=require('cors')
+const videoRoutes=require('./routes/videoRoutes.js')
+const port=3000;
+
+app.use(cors())
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use('/video', videoRoutes)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
